0b2e237a77d027f58fb4c0fa0b449ed4f295d2db
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-01-26 10:38:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -15,19 +15,58 @@ input.addEventListener('input', searchBox);
 function searchBox(e){
     e.preventDefault()
     debounce(() => {
-        const inputValue = e.input.value.trim();
+        const inputValue = e.target.value.trim();
         cleanHtml();
 
-        fetchCountries(countries)
-        .then()
-        .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
-
+        fetchCountries(inputValue )
+        .then(({countrys})=> {
+          if(countrys.length >= 10){
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+          }else if(countrys.length === 0){
+            Notiflix.Notify.info('Oops, there is no country with that name');
+          }
+          else if(countrys.length >= 2 && countrys.length <= 10){
+            Notiflix.Notify.info();
+          }
+          return countrys.reduse(
+            (markup, country) => createMarkup(country) + markup,
+            ""
+          )
+        })
+        .then(updateNewsList)
+        .catch(onError)
+        .finally(() => input.reset());
       }, DEBOUNCE_DELAY)
 }
 
+function createMarkup({name,capital,population,flags,languages}){
+  return `
+  <div class="country-card">
+  <img src=${flags} class="country-card" width="30" hight="20">
+  <h2 class="country-name">${name}</h2>
+  <h3 class="country-capital">${capital}</h3>
+  <p class="country-population">${population}</p>
+  <p class="country-languages">${languages}</p>
+  </div>
+  `;
+  
+}
+
+
+
+
+function updateNewsList(markup) {
+  document.getElementById("countryInfo").innerHTML = markup;
+}
+
+function onError(err) {
+  console.error(err);
+  
+}
 
 function cleanHtml() {
     if(inputValue === ''){
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
+    return;
   }}
